0e79af64828d08e0c42a9225695b1a1493101e35
root <roger.robinson@rms.com>
2018-11-19 15:39:44 +0000

    some updates

--- scripts/uge/prolog.sh
+++ scripts/uge/prolog.sh
@@ -8,15 +8,22 @@
 
 echo "---Running Prolog Script---"
 
+#echo "HELLO" 
+
+#TMPDIR="/mnt/gpfs/tmp/uge/${JOB_ID}.${SGE_TASK_ID}.${QUEUE}"
+#mkdir -p $TMPDIR
+
 echo "QUEUE=${QUEUE}"
 echo "TMPDIR=${TMPDIR}"
-echo "SGE_JOB_ID=${SGE_TASK_ID}"
+echo "JOB_ID=${JOB_ID}"
 echo "SGE_TASK_ID=${SGE_TASK_ID}"
 echo "SGE_O_WORKDIR=${SGE_O_WORKDIR}"
 echo "SGE_O_HOME=${SGE_O_HOME}"
 echo "HOSTNAME=${HOSTNAME}"
+echo "pe_hostfile ${pe_hostfile}"
 echo "PE_HOSTFILE=`cat $PE_HOSTFILE`"
 echo "PE_SLOTS=$PE_SLOTS"
+echo "SLOTS=${NSLOTS}"
 
 echo "---End Prolog Script---"
 
